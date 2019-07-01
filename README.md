3ea888cc36d55ffe92438ff459a4d61e4515bbbf
Wayne Patterson <Wayne.Patterson@nextgearcapital.com>
2019-07-01 11:17:40 -0400

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# Api Recorder
+
+A simple node server to record and return data from your api based on route.
+
+## Getting Started
+
+### Cloning
+To clone the project, run the following command.
+
+``` git clone git@ghe.coxautoinc.com:Wayne-Patterson/api-recorder.git ```
+
+### Usage
+
+```cd api-recorder```
+
+Create a `.env` file - sample included.
+
+```npm i``` to install dependencies.
+
+```node .``` will start the server.
---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fetch = require('node-fetch');
+cors = require('cors');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+require('dotenv').config();
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({ routes: [] })
+  .write();
+
+const app = express();
+
+app.use(cors());
+
+const forwardBaseUrl = process.env.FORWARD_BASE_URL;
+
+function forwardRequest(req, res) {
+  const { headers, body, method, url } = req;
+
+  const result = db.get('routes')
+    .find(url)
+    .value();
+
+  if(result) {
+    console.log('found it, returning stored value.');
+    return res.json(result[url]);
+  }
+
+  fetch(`${forwardBaseUrl}${url}`, {
+    method,
+    body,
+    headers: new fetch.Headers({
+      authorization: headers.authorization
+    })
+  })
+  .then(resp => resp.json())
+  .then(json => {
+    db.get('routes')
+      .push({ [url]: json })
+      .write();
+    console.log('wrote new route to db.');
+    return res.json(json);
+  });
+}
+
+app.get('*', forwardRequest);
+const port = process.env.port || 3080;
+app.listen(port);
